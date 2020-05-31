6026faf03d3a963dad35df712a77934933eaea2a
lkorchnoy <lkorchnoy@yahoo.com>
2020-05-31 15:11:12 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get '/', to: 'users#new'
+post '/', to: 'users#create'
+get '/login', to: 'sessions#new'
+post '/login', to: 'sessions#create'
+
+
+get '/welcome', to: 'welcome#show', as: 'homepage'
+
 end
